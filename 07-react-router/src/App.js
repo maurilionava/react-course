4fe5ba2293faddbb2577f7d63a16bddcf84d8290
Maurilio Nava Junior <mnavajunior@gmail.com>
2024-10-02 16:47:35 -0300

    Update App.js created page navigation navbar

--- 07-react-router/src/App.js
+++ 07-react-router/src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Home from './pages/Home'
 import About from './pages/About'
 
+import NavBar from './components/NavBar'
+
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+       <NavBar />
         <Routes>
           <Route  path="/" element={<Home />}/>
           <Route  path="/about" element={<About />}/>
