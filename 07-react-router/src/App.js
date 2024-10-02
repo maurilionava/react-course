4dd10fac0e056d441f896b75352f9ee7d4de28e5
Maurilio Nava Junior <mnavajunior@gmail.com>
2024-10-02 16:30:45 -0300

    Add react-router folder project

---
+++ 07-react-router/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+import Home from './pages/Home'
+import About from './pages/About'
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route  path="/" element={<Home />}/>
+          <Route  path="/about" element={<About />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
